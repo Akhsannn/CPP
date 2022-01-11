41ca0b0140649feb2631a5a02b7b5ad40bdb2514
Baloneyy <93771913+Baloneyy@users.noreply.github.com>
2022-01-12 04:15:16 +0700

    Update main.cpp

--- LOOPING/main.cpp
+++ LOOPING/main.cpp
@@ -1,41 +1,35 @@
 #include <iostream>
 
 using namespace std;
-string nama;
-int a,b,c,i;
+int x,y,z;
 int main()
 {
-    nama = "Muhammad Akhsan Nurramdhan";
-    cout << "=====Menu Looping=====" << endl;
-    cout << "1. Looping For" << endl;
-    cout << "2. Looping While" << endl;
-    cout << "3. Looping Do While" << endl;
-    cout << "Pilih Menu : ";
-    cin >> a ;
-    switch(a){
-    case 1 :
-        cout << "=====Menu Looping For=====" << endl;
-        cout << "1. Looping Nama" << endl;
-        cout << "2. Looping Bilangan Ganjil" << endl;
-        cout << "3. Looping Bilangan Genap" << endl;
-        cout << "Pilih Menu : " ;
-        cin >> b ;
-        switch(b){
-            case 1 :
-                for(i = 0 ; i < 5 ; i++){
-                cout << nama << endl;
-                }
-            case 2 :
-                for(i=1; i < 10 ; i++){
-                cout<< i + 2 << endl;
+    cout << "Menampilan Angka 1 s.d 5" << endl;
+    for (x = 1; x <= 5; x++){
+            for (y = 1; y <= x ; y++){
+                  cout << y ;
             }
-            case 3 :
-                for(i=1; i < 10; i++){
-                cout<< i + 1 << endl;
-                i++;
+            cout << endl ;
+      }
+    cout << endl ;
+    cout<<"Menampilkan Angka 5 s.d 1" <<endl;
+    for (x=5; x>=1; x--) {
+        for( y=0; y<=0+x; y++) {
+        cout<<" ";
+        }
+        for (z = x; z <= 5 ; z++){
+            cout << z ;
             }
-    }
-    }
+            cout<<endl;
+}
+    cout << endl ;
+    cout<<"Menampilkan Bintang, 5 baris & kolom" <<endl;
+    for (x = 1 ; x <=5 ; x++){
+        for(y = 0 ; y <= 5 ; y++){
+         cout<<"*";
+        }
+        cout<<endl;
+        }
+
     return 0;
 }
-
